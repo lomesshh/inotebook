cc355e09386a01a2867083423a34705af4339498
lomesh <badhelomesh82@gmail.com>
2021-11-19 12:04:18 +0530

    db set

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const url = "mongodb+srv://lomesh:lomesh123@cluster0.t8fzs.mongodb.net/test"
+
+const connectToMongo = () => {
+    mongoose.connect(url, () => {
+        console.log("Connected to mongo atlas successfully !");
+    })
+}
+
+module.exports = connectToMongo;
